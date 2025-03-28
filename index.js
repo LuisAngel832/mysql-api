b3137519d5684360adff73095831bd417689e428
Luis <luisgelipe.12340@gmail.com>
2025-03-28 10:44:55 -0600

    creacion del dokerfile

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 import server from './server/index.js';
 
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server is live at http://localhost:${PORT}`));
