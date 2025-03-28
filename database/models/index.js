73e020ed593e1f769dd59cf49d70e70c5ad46991
Luis <luisgelipe.12340@gmail.com>
2025-03-28 10:33:10 -0600

    api con mysql

---
+++ database/models/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+import { Sequelize } from 'sequelize';
+import process from 'process';
+import { fileURLToPath } from 'url';
+import config from '../config/config.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+const db = {};
+
+let sequelize;
+if (dbConfig.url) {
+  sequelize = new Sequelize(dbConfig.url, {
+    dialect: dbConfig.dialect,
+  });
+} else {
+  sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    import(path.join(__dirname, file)).then(module => {
+      const model = module.default(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    });
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
